b49edd65dc02ed077e9fe1c1c67ff82350c09c1b
Roman Skvortsov <roma.skvortsov@gmail.com>
2021-09-14 15:06:20 +0300

    Added db attributes

--- ShipsAndPorts.Core/Models/Ship.cs
+++ ShipsAndPorts.Core/Models/Ship.cs
@@ -1,19 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data.SqlTypes;
-using System.Data.Entity.Spatial;
+﻿using System.Data.Entity.Spatial;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ShipsAndPorts.Core.Models
 {
     public class Ship
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Required]
         public string ShipId { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public float Velocity { get; set; }
+        [Required]
         public DbGeography Geolocation { get; set; }
     }
 }
